b04a44bb30d3a91a6da3bb548671ec6308936d89
innerbit <suporte@innerbit.com.br>
2022-05-26 15:48:38 -0300

    Criado menu de instalacao de clientes

--- Install_Fusion.bat
+++ Install_Fusion.bat
@@ -3,52 +3,169 @@ REM - Executar como Administrador
 REM     if "%~s0"=="%~s1" ( cd %~sp1 & shift ) else (
 REM        echo CreateObject^("Shell.Application"^).ShellExecute "%~s0","%~0 %*","","runas",1 >"%tmp%%~n0.vbs" & "%tmp%%~n0.vbs" & del /q "%tmp%%~n0.vbs" & goto :eof
 REM )
-REM Cliente
-	cls
-	echo.
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo #            NOME DA EMPRESA/CLIENTE?               #
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo.
+REM VARIAVEIS
+	set instalador=cscript Fusion-Inventory.vbs
+	set 
+	
+	
+REM CLIENTES
+	:start 
+		CLS 
+		echo.
+		date /t
+		echo Computador: %computername% Usuario: %username% 
+		echo.
+		echo ###############################################
+		echo ######        Selecione Cliente          ######
+		echo ###############################################
+		echo. 
+		echo.
+		echo ===============================================
+		echo 	[1]. NEFRO CLINICA 
+		echo 	[2]. BINACIONAL
+		echo 	[3]. WASHINGTON TEIXEIRA
+		echo 	[4]. AUTO PECAS FOZ
+		echo 	[5]. CIOP
+		echo 	[6]. CLINICA HEALTH
+		echo 	[7]. ALTOMAX
+		echo 	[8]. INSTITO ETO
+		echo 	[9]. EB TURISMO
+		echo 	[10]. HOTEL SUICA
+		echo 	[11]. CELEIRO NATURAL
+		echo 	[12]. ULTRA IMAGEM
+		echo 	[13]. THUEDI
+		echo 	[14]. DELLANNO FOZ
+		echo 	[15]. POLICLINICA
+		echo 	[16]. SIGILUS CONTABILIDADE
+		echo.
+		echo 0. Sair
+		echo ==============================================
+		echo.
+		set /p choice="Escolha sua opcao: " 
+			if "%choice%"=="1" goto submenu_1 
+			if "%choice%"=="2" goto submenu_2
+			if "%choice%"=="2" goto submenu_3 
+			if "%choice%"=="2" goto submenu_4 
+			if "%choice%"=="2" goto submenu_5 
+			if "%choice%"=="2" goto submenu_6 
+			if "%choice%"=="2" goto submenu_7 
+			if "%choice%"=="2" goto submenu_8 
+			if "%choice%"=="2" goto submenu_9 
+			if "%choice%"=="2" goto submenu_10
+			if "%choice%"=="2" goto submenu_11
+			if "%choice%"=="2" goto submenu_12
+			if "%choice%"=="2" goto submenu_13
+			if "%choice%"=="2" goto submenu_14
+			if "%choice%"=="2" goto submenu_15
+    			
+			if "%choice%"=="0" goto exit
+			
+			:submenu_1
+			set empresa=NEFRO CLINICA
+			echo %empresa%
+			goto submenu_0
+			:submenu_2
+			set empresa=BINACIONAL
+			echo %empresa%
+			goto submenu_0
+			:submenu_3
+			set empresa=WASHINGTON
+			echo %empresa%
+			goto submenu_0
+			:submenu_4
+			set empresa=AUTO PECAS FOZ
+			echo %empresa%
+			goto submenu_0
+			:submenu_5
+			set empresa=CIOP
+			echo %empresa%
+			goto submenu_0
+			:submenu_6
+			set empresa=CLINICA HEALTH
+			echo %empresa%
+			goto submenu_0
+			:submenu_7
+			set empresa=ALTOMAX
+			echo %empresa%
+			goto submenu_0
+			:submenu_8
+			set empresa=INSTITO ETO
+			echo %empresa%
+			goto submenu_0
+			:submenu_9
+			set empresa=EB TURISMO
+			echo %empresa%
+			goto submenu_0
+			:submenu_10
+			set empresa=HOTEL SUICA
+			echo %empresa%
+			goto submenu_0
+			:submenu_11
+			set empresa=CELEIRO NATURAL
+			echo %empresa%
+			goto submenu_0
+			:submenu_12
+			set empresa=ULTRA IMAGEM
+			echo %empresa%
+			goto submenu_0
+			:submenu_13
+			set empresa=THUEDI
+			echo %empresa%
+			goto submenu_0
+			:submenu_14
+			set empresa=DELLANNO FOZ
+			echo %empresa%
+			goto submenu_0
+			:submenu_15
+			set empresa=POLICLINICA
+			echo %empresa%
+			goto submenu_0
+			:submenu_16
+			set empresa=SIGILUS CONTABILIDADE
+			echo %empresa%
+			goto submenu_0			
+			
 		set /p empresa="Empresa/Cliente: " 
 		echo %empresa%
-REM Instalar Fusion
-	cls
-	echo.
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo #            Instalando Fusion Inventory            #
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo.   
-        cscript Fusion-Inventory.vbs
-REM Instalar Fusion
-	cls
-	echo.
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo #            Fusion Inventory Instalado             #
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo.
-REM Copiar arquivo do anydesk
-	cls
-	echo.
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo #            Copiando Arquivo do AnyDesk para       #
-    echo #                   FusionInventory                 #
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo.
-        robocopy anydesk\ *AnyDesk.pm* C:\Tools\
-        xcopy C:\Tools\AnyDesk.pm "C:\Program Files\FusionInventory-Agent\perl\agent\FusionInventory\Agent\Task\Inventory\Generic\Remote_Mgmt" /f /l /-y
-        explorer C:\Program Files\FusionInventory-Agent\perl\agent\FusionInventory\Agent\Task\Inventory\Generic\Remote_Mgmt
-	cls
-	echo.
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo #            Arquivo AnyDesk Copiado                #
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo.
-    	cls
-	echo.
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo #              Abrir Fusion Status                  #
-	echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
-	echo.
-        start "Chrome" chrome --new-window http://localhost:62354
-pause
+			
+:submenu_0
+			REM Instalar Fusion
+				cls
+				echo.
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo #            Instalando Fusion Inventory            #
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo.   
+					cscript Fusion-Inventory.vbs
+			REM Instalar Fusion
+				cls
+				echo.
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo #            Fusion Inventory Instalado             #
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo.
+			REM Copiar arquivo do anydesk
+				cls
+				echo.
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo #            Copiando Arquivo do AnyDesk para       #
+				echo #                   FusionInventory                 #
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo.
+					robocopy anydesk\ *AnyDesk.pm* C:\Tools\
+					xcopy C:\Tools\AnyDesk.pm "C:\Program Files\FusionInventory-Agent\perl\agent\FusionInventory\Agent\Task\Inventory\Generic\Remote_Mgmt" /f /l /-y
+					explorer C:\Program Files\FusionInventory-Agent\perl\agent\FusionInventory\Agent\Task\Inventory\Generic\Remote_Mgmt
+				cls
+				echo.
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo #            Arquivo AnyDesk Copiado                #
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo.
+					cls
+				echo.
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo #              Abrir Fusion Status                  #
+				echo # - - - - - - - - - - - - - - - - - - - - - - - - - #
+				echo.
+					start "Chrome" chrome --new-window http://localhost:62354
+			pause
